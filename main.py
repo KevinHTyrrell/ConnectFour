32baf418473e550322b4cb33147a8dda57b9a424
KevinHTyrrell <KevinHTyrrell@gmail.com>
2022-01-31 21:03:41 -0500

    Modified to configure game between two bots then run 1000 games against each other

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from Bots.BaseBot import BaseBot
 from Game import Game
-
 
 if __name__ == "__main__":
     session = Game()
-    session.run_game()
+    grid_shape = session.get_grid_dims()
+    computer_player_one = BaseBot(grid_shape)
+    computer_player_two = BaseBot(grid_shape)
+    session.insert_bot(player=1, bot=computer_player_one)
+    session.insert_bot(player=2, bot=computer_player_two)
+
+    game = 0
+    while game < 1000:
+        session.run_game()
+        session.reset()
+        game += 1
